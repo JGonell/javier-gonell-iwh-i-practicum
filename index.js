fad5293ada0fd5bacf0d6298584ed42d877ac25f
Javier Gonell <javiergonell@MacBookAir.lan>
2025-03-31 11:17:02 +0200

    Final polished version with correct property handling and styling

--- index.js
+++ index.js
@@ -8,13 +8,12 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
+// Keep your token in a .env file
 const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
+// ROUTE 1 - Homepage: Display custom object data
 app.get('/', async (req, res) => {
-  const url = 'https://api.hubapi.com/crm/v3/objects/pets';
+  const url = 'https://api.hubapi.com/crm/v3/objects/pets?properties=name,type,age__years_';
 
   const headers = {
     Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
@@ -24,23 +23,24 @@ app.get('/', async (req, res) => {
   try {
     const response = await axios.get(url, { headers });
     const pets = response.data.results;
-    res.render('homepage', { title: 'Pets Table | HubSpot Practicum', pets });
+    res.render('homepage', {
+      title: 'Custom Object Table',
+      pets
+    });
   } catch (err) {
     console.error(err);
     res.status(500).send('Error retrieving pets');
   }
 });
 
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
+// ROUTE 2 - Show form to create/update object
 app.get('/update-cobj', (req, res) => {
   res.render('updates', {
     title: 'Update Custom Object Form | Integrating With HubSpot I Practicum'
   });
 });
 
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
+// ROUTE 3 - Handle form submission to create object
 app.post('/update-cobj', async (req, res) => {
   const petData = {
     properties: {
@@ -66,50 +66,7 @@ app.post('/update-cobj', async (req, res) => {
   }
 });
 
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
-});
-
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
-
-});
-*/
-
-
-// * Localhost
+// Start server
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+
+
